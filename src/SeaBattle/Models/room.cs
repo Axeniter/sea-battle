684ffabe9eb4e2038a552d75ba2bc2125ed836aa
lesluu <olesyaheky@gmail.com>
2026-01-21 20:09:05 +0300

    Создание класса Room для подключения по сети

---
+++ src/SeaBattle/Models/room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle.Models
+{
+    public class Room
+    {
+        public string IP { get; set; }
+        public string Name { get; set; }
+    }
+}
